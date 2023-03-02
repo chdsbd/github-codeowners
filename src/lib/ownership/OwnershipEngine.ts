60add9649bfd6666edbcfe6cea4a4116ecbabe5f
Christopher Dignam <chris@dignam.xyz>
2023-03-01 21:28:54 -0500

    line number information

--- src/lib/ownership/OwnershipEngine.ts
+++ src/lib/ownership/OwnershipEngine.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs';
-import ignore from 'ignore';
-import { FileOwnershipMatcher } from './types';
-import { log } from '../logger';
+import * as fs from "fs";
+import ignore from "ignore";
+import { FileOwnershipMatcher } from "./types";
+import { log } from "../logger";
 
 export class OwnershipEngine {
   private readonly matchers: FileOwnershipMatcher[];
@@ -13,7 +13,9 @@ export class OwnershipEngine {
     this.matchers = matchers;
   }
 
-  public calcFileOwnership(filePath: string): string[] {
+  public calcFileOwnership(
+    filePath: string
+  ): { lineno: number; owners: string[] } | null {
     // We reverse the matchers so that the first matching rule encountered
     // will be the last from CODEOWNERS, respecting precedence correctly and performantly
     const matchers = [...this.matchers].reverse();
@@ -21,15 +23,15 @@ export class OwnershipEngine {
     for (const matcher of matchers) {
       if (matcher.match(filePath)) {
         matcher.matched++;
-        return matcher.owners;
+        return { lineno: matcher.lineno, owners: matcher.owners };
       }
     }
 
-    return [];
+    return null;
   }
 
-  public getRules(): { rule: string, matched: number }[] {
-    const status: { rule: string, matched: number }[] = [];
+  public getRules(): { rule: string; matched: number }[] {
+    const status: { rule: string; matched: number }[] = [];
 
     for (const matcher of this.matchers) {
       status.push({ rule: matcher.rule, matched: matcher.matched });
@@ -38,19 +40,22 @@ export class OwnershipEngine {
     return status;
   }
 
-
   public static FromCodeownersFile(filePath: string) {
     try {
-      const lines = fs.readFileSync(filePath).toString().replace(/\r/g, '').split('\n');
+      const lines = fs
+        .readFileSync(filePath)
+        .toString()
+        .replace(/\r/g, "")
+        .split("\n");
 
       const owned: FileOwnershipMatcher[] = [];
 
-      for (const line of lines) {
-        if (!line || line.startsWith('#')) {
+      for (const [idx, line] of lines.entries()) {
+        if (!line || line.startsWith("#")) {
           continue;
         }
 
-        owned.push(createMatcherCodeownersRule(line));
+        owned.push(createMatcherCodeownersRule(line, idx));
       }
 
       return new OwnershipEngine(owned);
@@ -61,7 +66,10 @@ export class OwnershipEngine {
   }
 }
 
-const createMatcherCodeownersRule = (rule: string): FileOwnershipMatcher => {
+const createMatcherCodeownersRule = (
+  rule: string,
+  lineno: number
+): FileOwnershipMatcher => {
   // Split apart on spaces
   const parts = rule.split(/\s+/);
 
@@ -86,8 +94,11 @@ const createMatcherCodeownersRule = (rule: string): FileOwnershipMatcher => {
   // Workaround for rules ending with /*
   // GitHub will not look for nested files, so we adjust the node-ignore regex
   match._rules = match._rules.map((r: any) => {
-    if (r.pattern.endsWith('/*')) {
-      r.regex = new RegExp(r.regex.source.replace('(?=$|\\/$)', '(?=$|[^\\/]$)'), 'i');
+    if (r.pattern.endsWith("/*")) {
+      r.regex = new RegExp(
+        r.regex.source.replace("(?=$|\\/$)", "(?=$|[^\\/]$)"),
+        "i"
+      );
     }
     return r;
   });
@@ -97,10 +108,12 @@ const createMatcherCodeownersRule = (rule: string): FileOwnershipMatcher => {
     rule,
     path,
     owners: teamNames,
+    lineno,
     match: match.ignores.bind(match),
     matched: 0,
   };
 };
 
 // ensures that only the following patterns are allowed @octocat @octocat/kitty docs@example.com
-const codeOwnerRegex = /(^@[a-zA-Z0-9_\-/]*$)|(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const codeOwnerRegex =
+  /(^@[a-zA-Z0-9_\-/]*$)|(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
